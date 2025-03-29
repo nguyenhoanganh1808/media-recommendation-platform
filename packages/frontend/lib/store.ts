8d3eefce7fa8ddfe6e7011ecdf0d50a52d58b81f
zodd <1808nguyenhoanganh2003@gmail.com>
2025-03-29 09:48:29 +0700

    feat: add mediaReducer

--- packages/frontend/lib/store.ts
+++ packages/frontend/lib/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/auth/authSlice";
+import mediaReducer from "./features/media/mediaSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    media: mediaReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
